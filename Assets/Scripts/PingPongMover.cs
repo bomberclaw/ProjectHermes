f58e7289dc00810345fbc05b9c96a42d5f746470
Julian <julian_impelluso@hotmail.com>
2018-01-27 17:23:18 -0300

    Adding more prefabs Adding event pop-up

---
+++ Assets/Scripts/PingPongMover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongMover : MonoBehaviour
+{
+	public float movementDistance = 50;
+	public float movementDuration = 4;
+	public float durationOffset = 0;
+	public Vector3 movementVector;
+	public float delayOnGoal = 2;
+	private float counter;
+	private Vector3 startPosition;
+	private Vector3 goalPosition;
+	private Transform myTransform;
+
+	private void Start()
+	{
+		myTransform = transform;
+		movementVector.Normalize();
+		StartCoroutine(Move());
+	}
+	
+	private IEnumerator Move()
+	{
+		counter = durationOffset;
+		while (true)
+		{
+			startPosition = myTransform.position;
+			goalPosition = startPosition + movementVector * movementDistance;
+			while (counter < movementDuration)
+			{
+				myTransform.position = Vector3.Lerp(startPosition, goalPosition, counter / movementDuration);
+				counter += Time.deltaTime;
+				yield return new WaitForEndOfFrame();
+			}
+			myTransform.position = goalPosition;
+			movementVector = -movementVector;
+
+			counter = 0;
+			yield return new WaitForSeconds(delayOnGoal);
+
+			goalPosition = myTransform.localScale;
+			goalPosition.x = -goalPosition.x;
+			myTransform.localScale = goalPosition;
+		}
+	}
+}
