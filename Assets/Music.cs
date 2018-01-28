59688f2302eec2662a39e56a2062e3d131ebbca4
Julian <julian_impelluso@hotmail.com>
2018-01-28 17:01:45 -0300

    Added menu buttons and "how to play" text Fixed music

---
+++ Assets/Music.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+	static private Music instance;
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			DontDestroyOnLoad(this.gameObject);
+			instance = this;
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
